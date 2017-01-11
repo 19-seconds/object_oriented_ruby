2f968fb5159847f2ccdfd784bf3cdc26e22e9a03
Mark Richardson <markeban@yahoo.com>
2017-01-11 14:09:34 +0200

    added employee example of arrays and hashes

---
+++ arrays_and_hashes.rb
@@ -0,0 +1,5 @@
+employee1 = {"first_name" => "Majora", "last_name" => "Carter", "salary" => 20000, "active" => true}
+employee2 = {"first_name" => "Dan", "last_name" => "Campos", "salary" => 15000, "active" => true}
+
+puts "salary is " + employee2["salary"].to_s
+puts "salary is #{employee2["salary"]}" 
